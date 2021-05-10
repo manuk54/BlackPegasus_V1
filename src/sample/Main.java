404ce9b18280ede2a6989d754be0d7304a55ae61
manuk54 <baidarka28@gmail.com>
2021-05-10 09:44:32 +0200

    HelloLabel

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello Darkness!");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene = new Scene(root, 1200, 600);
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
